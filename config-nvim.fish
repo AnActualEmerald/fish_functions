717579ceb1a8a8ff64dca9a610ec62f5e281a570
AnActualEmerald <kevinliger13@gmail.com>
2021-07-29 10:21:02 -0400

    Auto commit 07/29/21

---
+++ config-nvim.fish
@@ -0,0 +1,3 @@
+function config-nvim --wraps='nvim ~/.config/nvim/init.vim' --description 'alias config-nvim nvim ~/.config/nvim/init.vim'
+  nvim ~/.config/nvim/init.vim $argv; 
+end
---
+++ gotop.fish
@@ -0,0 +1,3 @@
+function gotop --description 'alias gotop gotop --nvidia'
+ command gotop --nvidia $argv; 
+end
